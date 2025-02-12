db745152587a005870fd0453913a7761b624cc6a
Martin-Mc-Donnell <martinmcdonnell360@hotmail.com>
2025-02-12 08:23:07 +0000

    fix: Removed unused code from requests_test.py

--- requests_test.py
+++ requests_test.py
@@ -5,7 +5,5 @@ try:
     res.raise_for_status()
 except Exception as exc:
     print('There was a problem: %s' % (exc))
-print(type(res))
-res.status_code == requests.codes.ok
 print(len(res.text))
 print(res.text[:250])
